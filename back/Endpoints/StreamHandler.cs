3d3a035b05ff5d0baba98adec81a4c927dbcfba3
Christophe Petitjean <christophe@famille-petitjean.com>
2025-05-07 14:43:08 +0200

    cookie does not works with events

---
+++ back/Endpoints/StreamHandler.cs
@@ -0,0 +1,64 @@
+using System.Net.ServerSentEvents;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using EventStreamBrowser.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventStreamBrowser.Endpoints;
+
+public class StreamHandler
+{
+    public static async Task<IResult> HandleStream(
+        HttpContext context,
+        CancellationToken token,
+        [FromServices] ChannelFactory channelFactory,
+        [FromServices] ILogger<StreamHandler> logger)
+    {
+        // Retrieve the ClientId from the request cookie if exists, or from the response headers
+        string? clientId = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        if (string.IsNullOrEmpty(clientId))
+        {
+            logger.LogWarning("Client ID is missing in the request.");
+            return Results.BadRequest("Client ID is missing.");
+        }
+
+        // Get the channel for the specific client
+        var channel = channelFactory.GetChannel(clientId);
+
+        // if client close connexion, remove the channel
+        token.Register(() =>
+        {
+            logger.LogInformation("Cancellation requested for client {ClientId}.", clientId);
+            channelFactory.RemoveChannel(clientId);
+        });
+
+        // Return the SSE stream
+        return TypedResults.ServerSentEvents(StreamSseItems(token, logger, clientId, channel));
+    }
+
+    private static async IAsyncEnumerable<SseItem<WeatherForecast>> StreamSseItems(
+        [EnumeratorCancellation] CancellationToken cancellationToken,
+        ILogger logger,
+        string? clientId,
+        System.Threading.Channels.Channel<WeatherForecast>? channel)
+    {
+        logger.LogInformation("Starting SSE stream for client {ClientId}.", clientId);
+
+        while (!cancellationToken.IsCancellationRequested
+                && channel is not null
+                && await channel.Reader.WaitToReadAsync(cancellationToken))
+        {
+            // Read the next WeatherForecast from the channel
+            var forecast = await channel.Reader.ReadAsync(cancellationToken);
+            logger.LogInformation("Sending weather forecast to client {ClientId}: {Forecast}", clientId, forecast);
+
+            // Yield the forecast as an SSE item
+            yield return new SseItem<WeatherForecast>(forecast, "weatherforecast")
+            {
+                ReconnectionInterval = TimeSpan.FromSeconds(30),
+            };
+        }
+
+        logger.LogInformation("SSE stream for client {ClientId} has ended.", clientId);
+    }
+}
--- back/Middleware/CookieMiddleware.cs
+++ back/Middleware/CookieMiddleware.cs
@@ -1,3 +1,6 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
 public class CookieMiddleware(RequestDelegate next, ILogger<CookieMiddleware> logger)
 {
     private readonly ILogger<CookieMiddleware> _logger = logger;
@@ -10,12 +13,10 @@ public class CookieMiddleware(RequestDelegate next, ILogger<CookieMiddleware> lo
         {
             _logger.LogInformation("ClientId cookie not found. Generating a new one.");
             clientId = Guid.NewGuid().ToString();
-            context.Response.Cookies.Append(CookieName, clientId, new CookieOptions
-            {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict
-            });
+            context.User.AddIdentity(new ClaimsIdentity(
+            [
+                new Claim(ClaimTypes.NameIdentifier, clientId)
+            ]));
         }
         else
         {
